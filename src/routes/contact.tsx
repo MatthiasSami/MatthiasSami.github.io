e09736a293df4eca2ff383c14bdd2087f7b619b5
Matthias Sami <matthias.sami@etu.unistra.fr>
2025-09-03 09:49:34 +0200

    Ajout des cartes de navigation

--- src/routes/contact.tsx
+++ src/routes/contact.tsx
@@ -5,6 +5,9 @@ export default function Contact() {
         <p>Vous pouvez me contacter via les réseaux sociaux suivants :</p>
         <ul>
             <li><a href="https://www.linkedin.com/in/matthias-sami/" target="_blank" rel="noopener noreferrer">LinkedIn</a></li>
+            <li><a href="https://github.com/MatthiasSami" target="_blank" rel="noopener noreferrer">GitHub</a></li>
+            <li><a href="mailto:samimatthias@gmail.com">Email</a></li>
+            <li><a href="tel:+33644616234">Téléphone</a></li>
         </ul>
     </div>
     </>);
--- src/routes/home.tsx
+++ src/routes/home.tsx
@@ -1,11 +1,36 @@
+import { NavLink } from "react-router-dom";
+
 export default function Home() {
     return (<>
     <div className="home">
-        <img src="/moi.jpg" alt="Photo de moi" className="imageMoiRond" />
-        <div className="presentation">
-            <h2>Salut, je suis Matthias SAMI 👋</h2>
-            <p>Etudiant en 3ème année de BUT Informatique,<br />
-            passioné par l'informatique et plus particulièrement du développement web et logiciel.</p>
+        <div className="intro">
+            <img src="/moi.jpg" alt="Photo de moi" className="imageMoiRond" />
+            <div className="presentation">
+                <h2>Salut, je suis Matthias SAMI 👋</h2>
+                <p>Etudiant en 3ème année de BUT Informatique,<br />
+                passioné par l'informatique et plus particulièrement du développement web et logiciel.</p>
+            </div>
+        </div>
+            
+        <div className="cards">
+
+            <NavLink to="/projets" className="card">
+                <img src="/programming.jpg" alt="Mes projets" className="card-img" />
+                <h3>🚀 Mes projets</h3>
+                <p>Découvrez mes projets en développement web, C, Java et plus encore.</p>
+            </NavLink>
+
+            <NavLink to="/a-propos" className="card">
+                <img src="/about-me.jpg" alt="À propos de moi" className="card-img" />
+                <h3>👤 À propos</h3>
+                <p>En savoir plus sur mon parcours et mes centres d’intérêt.</p>
+            </NavLink>
+
+            <NavLink to="/contact" className="card">
+                <img src="/contact.jpg" alt="Contactez-moi" className="card-img" />
+                <h3>📞 Contact</h3>
+                <p>Restons en contact via LinkedIn, GitHub ou email.</p>
+            </NavLink>
         </div>
     </div>
     </>);
